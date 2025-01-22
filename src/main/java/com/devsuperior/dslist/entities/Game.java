3d95bdfea4346a74538062b7247cc384b8111dc0
Adalto dos Reis Junior <adalto.junior@gmail.com>
2025-01-22 11:37:14 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -19,25 +19,27 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-
     private String platforms;
-    private String imgURL;
+    private Double score;
+    private String imgUrl;
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgURL,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
-        this.imgURL = imgURL;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
+        this.score = score;
     }
 
     public Long getId() {
@@ -80,12 +82,20 @@ public class Game {
         this.platforms = platforms;
     }
 
-    public String getImgURL() {
-        return imgURL;
+    public Double getScore() {
+        return score;
     }
 
-    public void setImgURL(String imgURL) {
-        this.imgURL = imgURL;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getimgUrl() {
+        return imgUrl;
+    }
+
+    public void setimgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
